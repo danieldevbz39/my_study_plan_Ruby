c61c0c0c3ac6ebcb02cf6f68175c790391ab08d1
Daniel Siqueira <danielbzdigital@outlook.com>
2024-03-18 18:36:58 -0300

    search s2

---
+++ Algoritimos/Challenges/Level 1/breadth_first_search.rb
@@ -0,0 +1,59 @@
+
+## Breadth First Search algorithm.
+
+def breadth_first_search(adj_matrix, source, terminal)
+    node_array = [source]
+    path = []
+    puts "The initial node_array is #{node_array}."
+  
+    loop do
+      curr_node = node_array.pop
+      path << curr_node
+      puts "The next node to be checked is #{curr_node}."
+      if curr_node.nil?
+        puts 'Terminal node was not found!'
+        return false
+      elsif curr_node == terminal
+        puts "Terminal node #{curr_node} found!"
+        puts "The path between node #{source} and node #{terminal} is: #{path}."
+        return true
+      end
+  
+      puts "Iterating through the following array:  #{adj_matrix[curr_node]}"
+      children = (0..adj_matrix.length - 1).to_a.select do |i|
+        puts "Checking the #{i} index which value #{adj_matrix[curr_node][i]}"
+        adj_matrix[curr_node][i] == 1
+      end
+      puts "The children array returned: #{children}"
+      node_array = children + node_array
+      puts "After appending children to node_array, node_array is #{node_array}."
+    end
+  end
+  
+  ## Adjacency Matrix
+  
+  adj_matrix =
+    [
+      [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+  
+  # Passing the adjacent matrix, source, and terminal as arguments to the algorithm.
+  breadth_first_search(adj_matrix, 0, 17)
+  
