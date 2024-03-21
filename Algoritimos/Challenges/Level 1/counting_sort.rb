66a4f2bb13a42f9550a9767145f0061ebb437211
Daniel Siqueira <danielbzdigital@outlook.com>
2024-03-20 21:40:36 -0300

    20/3/24

--- Algoritimos/Challenges/Level 1/counting_sort.rb
+++ Algoritimos/Challenges/Level 1/counting_sort.rb
@@ -1,37 +1,31 @@
-def counting_sort(array)
-	## step 1
-	max_value = array.max
+def counting_sort(a=[9,8,7,6],min=0,max=10)
+    if min>max
+      return "invalid range"
+    end
+    
+  	n=max-min+1
+	count=Array.new(n,0)
+	len=a.length
+	output=Array.new(len)
   
-	## step 2
-	count_array = Array.new(max_value - 1, 0)
-  
-	## step 3
-	array.each { |n| count_array[n] += 1 }
-  
-	## step 4
-	k = 1
-	
-	while k < count_array.size
-	  count_array[k] += count_array[k - 1]
-  
-	  k += 1
+  	for i in 0...len
+		count[a[i]-min]+=1
 	end
 	
-	## step 5
-	s = array.size - 1
-	
-	## step 6
-	output_array = []
-  
-	while s >= 0
-	  ## step 7
-	  output_array[count_array[array[s]] - 1] = array[s]
-	  
-	  ## step 8
-	  count_array[array[s]] -= 1
-  
-	  s -= 1
+  	for i in 1...n
+		count[i]+=count[i-1]
 	end
-  
-	output_array
-  end
+		
+	
+	for i in 0...len
+		output[count[a[i]-min]-1]=a[i]
+		count[a[i]-min]-=1
+	end
+	
+	for i in 0...len
+		a[i]=output[i]
+	end
+	
+	return a
+	
+end
